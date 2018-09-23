54868ad80c947a8464c2f1c523030f543f8cd7f5
Philippe Deslongchamps <philippe.deslongchamps@gmail.com>
2018-09-23 12:10:53 -0400

    adde move to wrapper

---
+++ moveToWrapper.sh
@@ -0,0 +1,8 @@
+cp build "C:\Users\Philippedesp08089209\source\repos\StockGazeWrapper\StockGazeWrapper\ClientApp" -r
+cp config "C:\Users\Philippedesp08089209\source\repos\StockGazeWrapper\StockGazeWrapper\ClientApp" -r
+cp public "C:\Users\Philippedesp08089209\source\repos\StockGazeWrapper\StockGazeWrapper\ClientApp" -r
+cp src "C:\Users\Philippedesp08089209\source\repos\StockGazeWrapper\StockGazeWrapper\ClientApp" -r
+cp static "C:\Users\Philippedesp08089209\source\repos\StockGazeWrapper\StockGazeWrapper\ClientApp" -r
+cp package.json "C:\Users\Philippedesp08089209\source\repos\StockGazeWrapper\StockGazeWrapper\ClientApp"
+cp index.html "C:\Users\Philippedesp08089209\source\repos\StockGazeWrapper\StockGazeWrapper\ClientApp"
+cp .eslintrc.js "C:\Users\Philippedesp08089209\source\repos\StockGazeWrapper\StockGazeWrapper\ClientApp"
